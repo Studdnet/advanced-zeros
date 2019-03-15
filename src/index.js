1cf231e1782253ab20bd1a9453ce8dcb191e9920
Studdnet <St.codding@gmail.com>
2019-03-15 10:13:41 +0300

    docs: I need to disassemble this code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+k = base;
+  plains = [];
+  for (var i = 2; i <= base;) {
+    if (k % i == 0) {
+      if (isNaN(
+        plains[i])) {
+
+        plains[i] = 0;
+      }
+      plains[i]++;
+      k = k / i;
+    }
+    else {
+      i++;
+    }
+  }
+  var answer = [];
+  for (var i = 0; i <= base; ++i) {
+    if (!isNaN(
+      plains[i])) {
+      var temp = i, res = 0;
+      while (number / temp) {
+        res = res + Math.floor(number / temp);
+        temp = temp * i;
+      }
+      answer.push(Math.floor(res /
+        plains[i]));
+    }
+  }
+  return arrayMin(answer);
+}
+function arrayMin(array) {
+  return array.reduce((a, b) => Math.min(a, b));
 }
